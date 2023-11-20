80aab30f3f9c60379a7c3dec7cbbd286939cb011
Oliver Unasing <oliverunasing@hotmail.com>
2023-11-20 20:22:36 +0000

    Updates milestone_2.py, task 3 complete, code is now seperated into functions, much cleaner

--- milestone_2.py
+++ milestone_2.py
@@ -4,14 +4,27 @@ word_list = ['Apple', 'Banana', 'Pear', 'Kiwi', 'Lemon']
 secret_word = random.choice(word_list).lower()
 print(secret_word)
 
-while True:
-    guess = input('Please enter your guess:').lower()
-    if guess.isalpha():
-        print('Good guess!')
-        if guess in secret_word:
-            print(f'{guess} is in the secret word.')
-        else:
-            print(f'Sorry, {guess} is not in the word. Try again.')
-        break
+
+
+
+def check_guess(guess):
+    if guess in secret_word:
+        print(f'{guess} is in the secret word.')
     else:
-        print('Invalid letter. Please enter a single alphabetical character.')
+        print(f'Sorry, {guess} is not in the word. Try again.')
+
+    return 
+
+def ask_for_input():
+    while True:
+        guess = input('Please enter your guess:').lower()
+        if guess.isalpha():
+            print('Good guess!')
+            check_guess(guess)
+            break
+        else:
+            print('Invalid letter. Please enter a single alphabetical character.')
+    
+    return guess
+
+ask_for_input()
