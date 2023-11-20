901b4c6d8dd962b9be488f25bf813ba1cbb0b209
Oliver Unasing <oliverunasing@hotmail.com>
2023-11-20 21:10:31 +0000

    updated milestone_4.py, task 2 completed, methods check_guess and ask_for_input created and currently WIP

--- milestone_4.py
+++ milestone_4.py
@@ -9,7 +9,25 @@ class Hangman:
         self.num_letters = len(set(self.word))
         self.list_of_guesses = list_of_guesses or []
 
+    def check_guess(self, guess):
+        guess = guess.lower()
+        if guess in self.word.lower():
+            print(f'Good guess! {guess} is in the word.')
+    
+
         
+
+    def ask_for_input(self):
+        while True:
+            guess = input('Please enter your guess:')
+            if guess.isalpha() == False:
+                print('Invalid letter. Please enter a single alphabetical character.')
+            elif guess in self.list_of_guesses:
+                print('You already tried that letter.')
+            else: 
+                self.check_guess(guess)
+    
+
 
 
 game1 = Hangman(['Apple', 'Banana', 'Pear', 'Kiwi', 'Lemon'])
@@ -19,3 +37,5 @@ print(game1.word)
 print(game1.word_guessed)
 print(game1.num_letters)
 print(game1.list_of_guesses)
+
+game1.ask_for_input()
